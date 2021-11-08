5edd82a393424778aef9357fb55b70229ee0decc
Yunjong-Boo <boo0117@naver.com>
2021-11-08 19:29:55 +0900

    version 2

--- reverse.c
+++ reverse.c
@@ -15,12 +15,12 @@ typedef struct _ListStack{
 void Push(ListStack* stack, char *line){
 	Node* new = malloc(sizeof(Node));
 	if(new == NULL){
-		printf("malloc failed\n");
+		fprintf(stderr, "malloc failed\n");
 		exit(1);
 	}
 	new->data = (char*)malloc((sizeof(line)/sizeof(char)));
 	if(new->data == NULL){
-		printf("malloc failed\n");
+		fprintf(stderr, "malloc failed\n");
 		exit(1);
 	}
 	strcpy(new->data, line);
@@ -42,25 +42,25 @@ void Pop(ListStack* stack, FILE *ofp){
 
 int main(int argc, char **argv){
 	ListStack *Stack = calloc(1, sizeof(ListStack));
+	char *input_line = NULL;
+	size_t len = 0;
+	ssize_t nread;
 
 	/* if input two command-line arguments(input.txt & output.txt) */
 	if(argc == 3){
 		/* if input, output file is same -> print error message */
 		if(strcmp(argv[1], argv[2]) == 0){
-			printf("Input and output file must differ\n");
+			fprintf(stderr, "reverse: input and output file must differ\n");
 			exit(1);
 		}
 
 		FILE *ifp;
 		/* if failed to open input file -> print error message */
 		if((ifp = fopen(argv[1], "r")) == NULL){
-			printf("error: cannot open file 'input.txt'\n");
+			fprintf(stderr, "reverse: cannot open file '%s'\n", argv[1]);
 			exit(1);
 		}
 
-		char *input_line = NULL;
-		size_t len = 0;
-		ssize_t nread;
 		/* getline until file EOF */
 		while((nread = getline(&input_line, &len, ifp)) != -1){
 			Push(Stack, input_line);
@@ -68,7 +68,7 @@ int main(int argc, char **argv){
 
 		FILE *ofp;
 		if((ofp = fopen(argv[2], "w")) == NULL){
-			printf("error: cannot open file 'output.txt'\n");
+			fprintf(stderr, "error: cannot open file 'output.txt'\n");
 			exit(1);
 		}
 		/* print line in output file to reverse order */
@@ -76,13 +76,28 @@ int main(int argc, char **argv){
 		for(int i = 0; i < size; i++){
 			Pop(Stack, ofp);
 		}
-		free(input_line);
 		fclose(ifp);
 		fclose(ofp);
 	}
 	/* if input one command-line argument(input.txt) */
 	else if(argc == 2){
-		
+		FILE *ifp;
+		if((ifp = fopen(argv[1], "r")) == NULL){
+			fprintf(stderr, "reverse: cannot open file '%s'\n", argv[1]);
+			exit(1);
+		}
+
+		/* getline until file EOF */
+		while((nread = getline(&input_line, &len, ifp)) != -1){
+			Push(Stack, input_line);
+		}
+
+		/* print line in stdout to reverse order */
+		int size = Stack->size;
+		for(int i = 0; i < size; i++){
+			Pop(Stack, stdout);
+		}
+		fclose(ifp);
 	}
 	/* if no input argument */
 	else if(argc == 1){
@@ -90,10 +105,11 @@ int main(int argc, char **argv){
 	}
 	/* too many arguments passed to program */
 	else{
-		printf("usage: reverse <input> <output>\n");
+		fprintf(stderr, "usage: reverse <input> <output>\n");
 		exit(1);
 	}
 
+	free(input_line);
 	free(Stack);
 	return 0;
 }
